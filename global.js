b243a6b289f01f0fe93c55b5aad582b9788cafa4
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-02 14:20:25 -0600

    further deconstructed

--- global.js
+++ global.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 
-//assign the global object process's property versions to a variable
-let myVersions = process.versions;
+
 //using deconstruciton, take the values of key's in that object and
 //assign them to new variables
-var {node: myNode, v8: myV8} = myVersions;
+
+let {node: myNode, v8: myV8} = process.versions;
+
 
 //use string templating to create a console log
 console.log(`Node.js version: ${myNode}\nV8 version: ${myV8}`);
