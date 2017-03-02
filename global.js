54de687d46ec89d1a425df9bcc667ec2680923bc
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-02 14:07:47 -0600

    console log achieved

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 let nodeJsVersion = process.versions.node;
 let v8version = process.versions.v8;
-console.log(nodeJsVersion);
+console.log(`Node.js version: ${nodeJsVersion}\nV8 version: ${v8version}`);
